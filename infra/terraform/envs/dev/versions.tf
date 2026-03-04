edaa43049d81dfddbf2f264021ba19e7603ffdd0
Leandro Santos <leandrolps@gmail.com>
2026-03-04 07:59:41 -0300

    Configure Terraform remote state backend (S3 + DynamoDB lock)

--- infra/terraform/envs/dev/versions.tf
+++ infra/terraform/envs/dev/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "urbanflow-tfstate-us-east-1-139961319000"
+    key            = "urbanflow/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "urbanflow-tf-lock-us-east-1"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
