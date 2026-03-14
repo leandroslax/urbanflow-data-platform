6ef868d86aed747f9d3177020ec808aa135bd3ef
Leandro Santos <leandrolps@gmail.com>
2026-03-14 09:51:36 -0300

    Rename climate gold stream pipeline to normalized name

--- scripts/start_gold_clima.sh
+++ scripts/start_gold_clima.sh
@@ -6,5 +6,5 @@ mkdir -p "${LOG_DIR}"
 
 exec /home/ec2-user/spark/bin/spark-submit \
   --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
-  /home/ec2-user/urbanflow-data-platform/jobs/gold/stream_clima_silver_to_gold_v1.py \
+  /home/ec2-user/urbanflow-data-platform/jobs/gold/stream_clima_silver_to_gold.py \
   >> "${LOG_DIR}/gold_clima.log" 2>&1
